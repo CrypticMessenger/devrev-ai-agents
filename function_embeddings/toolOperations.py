4ff43c36b919bf838737558511307ec98f460cd0
Ankit <2020csb1072@iitrpr.ac.in>
2023-12-11 23:37:04 +0530

    add tool script

---
+++ function_embeddings/toolOperations.py
@@ -0,0 +1,41 @@
+from .connectdb import connectdb, disconnectdb
+
+def get_all_tools(model:str,output_fields=['function_name', 'description', 'embedding', 'examples','arguments'])->list:
+    try:
+        # connect to db
+        collection = connectdb(model)
+        collection.load()
+        # get all tools
+        res = collection.query(
+            expr = 'function_name>""', #get all entries to true, to get all functions
+            output_fields = output_fields,
+            limit=10000
+        )
+        collection.release()
+        # disconnect from db
+        disconnectdb()
+        # return all tools
+        return res
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+        return None
+    
+def search_tool(model:str, tool_name:str, output_fields=['function_name', 'description', 'embedding', 'examples','arguments'])->list:
+    try:
+        # connect to db
+        collection = connectdb(model)
+        collection.load()
+        # get all tools
+        res = collection.query(
+            expr = f'function_name=="{tool_name}"',
+            output_fields = output_fields,
+            limit=10
+        )
+        collection.release()
+        # disconnect from db
+        disconnectdb()
+        # return all tools
+        return res
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+        return None
